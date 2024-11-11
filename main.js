4d61abf27ea7ded0de8898a0dbc4359e75d57190
Manuella-R <67600619+Manuella-R@users.noreply.github.com>
2024-11-11 10:59:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+// Mobile Menu Toggle
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+// Toggle the "open" class when the menu button is clicked
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+// Close the mobile menu when any nav link is clicked
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+// ScrollReveal configuration for animations
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// ScrollReveal Animations
+ScrollReveal().reveal(".container__left h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".container__left .container__btn", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".container__right h4", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".container__right h2", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+ScrollReveal().reveal(".container__right h3", {
+  ...scrollRevealOption,
+  delay: 3000,
+});
+ScrollReveal().reveal(".container__right p", {
+  ...scrollRevealOption,
+  delay: 3500,
+});
+ScrollReveal().reveal(".container__right .tent-1", {
+  duration: 1000,
+  delay: 4000,
+});
+ScrollReveal().reveal(".container__right .tent-2", {
+  duration: 1000,
+  delay: 4500,
+});
+ScrollReveal().reveal(".location", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 5000,
+});
+ScrollReveal().reveal(".socials span", {
+  ...scrollRevealOption,
+  origin: "top",
+  delay: 5500,
+  interval: 500,
+});
+
+// Lightbox Functions
+// Function to open the lightbox and display the clicked image in full screen
+function openLightbox(imageId) {
+  var lightbox = document.getElementById("lightbox");
+  var lightboxImg = document.getElementById("lightbox-img");
+
+  // Update the source of the lightbox image based on the clicked image
+  lightboxImg.src = document.getElementById(imageId).src;
+
+  // Show the lightbox
+  lightbox.style.display = "flex";
+}
+
+// Function to close the lightbox
+function closeLightbox() {
+  var lightbox = document.getElementById("lightbox");
+
+  // Hide the lightbox
+  lightbox.style.display = "none";
+}
